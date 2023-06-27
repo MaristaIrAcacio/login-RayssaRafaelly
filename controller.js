830c5fdb826ccb2939322e39e92fab71bfabcf9a
Rayssa Rafaelly <rayssa.kopinski@alunoescolassociais.org.br>
2023-06-27 11:40:54 -0300

    arquivos modificados pq estavam duplicados

--- controller.js
+++ controller.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function acessar() {
     let loginEmail = document.getElementById("loginEmail").value;
     let loginSenha = document.getElementById("loginSenha").value;
@@ -9,16 +8,4 @@ function acessar() {
         window.location.href = "aula2.html";
 		console.log(window.location.href);
     }
-=======
-function acessar() {
-    let loginEmail = document.getElementById("loginEmail").value;
-    let loginSenha = document.getElementById("loginSenha").value;
- 
-    if(!loginEmail || !loginSenha) {
-        alert("favor preencha os campos para acessar o site");
-    } else {
-        window.location.href = "aula2.html";
-		console.log(window.location.href);
-    }
->>>>>>> 0bd4a55b70e9e30c96005b7f10b3c859f3f0630d
 }
